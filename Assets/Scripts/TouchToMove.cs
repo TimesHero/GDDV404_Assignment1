66b20aa1edc724bad1e9c1d974e20b91026a6b57
AJ-FW16-CachyOS <alexander.aj.fraser@gmail.com>
2026-01-22 20:06:34 -0500

    Initial Unity Project Commit

---
+++ Assets/Scripts/TouchToMove.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TouchToMove : MonoBehaviour
+{
+     public InputSystem_Actions inputActions;
+    public UnityEngine.AI.NavMeshAgent navAgent;
+       
+
+        public void Awake()
+        {
+            inputActions = new InputSystem_Actions();
+        }
+
+        public void OnEnable()
+        {
+            inputActions.Enable();
+            inputActions.Player.Attack.performed += OnAttack;
+        }
+
+        public void OnAttack(InputAction.CallbackContext context)
+        {
+            Vector2 screenPos = inputActions.Player.Point.ReadValue<Vector2>();
+
+            Ray mouseRay = Camera.main.ScreenPointToRay(screenPos);
+
+            if (Physics.Raycast(mouseRay, out RaycastHit hitInfo))
+            {
+                navAgent.SetDestination(hitInfo.point);
+            }
+        }
+
+        
+        public void OnDisable()
+        {
+            inputActions.Disable();
+            inputActions.Player.Attack.performed -= OnAttack;
+        }
+    
+}
